30379a914a1cde72b0e0520a88069e7ff48b0204
RLRama <rl.ramiro11@gmail.com>
2025-05-06 12:48:15 -0300

    feat (WIP): continue task CRUD frontend development)

--- src/routes/tasks/+page.svelte
+++ src/routes/tasks/+page.svelte
@@ -3,7 +3,7 @@
     import {goto} from "$app/navigation";
     import {user} from "$lib/stores.js"
     import {apiRequest} from "$lib/api.js";
-    import {Alert} from "@sveltestrap/sveltestrap";
+    import {Alert, Form, FormGroup, Input, Label} from "@sveltestrap/sveltestrap";
 
     let tasks = [];
     let newTask = { title: '', description: '', completed: false, category_id: 0, due_date: '', tag_ids: [] };
@@ -54,3 +54,22 @@
 {/if}
 
 <h2>Create task</h2>
+
+<Form on:submit={() => createTask()}>
+    <FormGroup>
+        <Label for="title">Title</Label>
+        <Input type="text" id="title" bind:value={newTask.title} required />
+    </FormGroup>
+    <FormGroup>
+        <Label for="description">Description</Label>
+        <Input type="text" id="description" bind:value={newTask.description} />
+    </FormGroup>
+    <FormGroup>
+        <Label for="completed">Completed</Label>
+        <Input type="checkbox" id="completed" bind:value={newTask.completed} />
+    </FormGroup>
+    <FormGroup>
+        <Label for="description">Category</Label>
+        <Input type="text" id="description" bind:value={newTask.description} />
+    </FormGroup>
+</Form>
